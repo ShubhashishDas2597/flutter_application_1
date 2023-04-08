43dcba454280e7f967d2270b9d3095eb970ff6d7
Shubhashish Das <44511116+ShubhashishDas2597@users.noreply.github.com>
2023-04-08 23:43:40 +0530

    Create technical.dart

---
+++ lib/technical.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class technical extends StatelessWidget {
+  const technical({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Tech");
+  }
+}
